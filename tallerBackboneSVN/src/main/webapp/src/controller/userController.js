5c11f4a88076507b7ee23c5f4b61a8924e53ff73
alexlo95 <jlozada1605@hotmail.com>
2014-08-12 23:54:00 -0500

    Mi primer commit

---
+++ tallerBackboneSVN/src/main/webapp/src/controller/userController.js
@@ -0,0 +1,43 @@
+   
+    App.Controller.UserController = Backbone.View.extend({
+        el: '#main',
+        initialize: function(options) {
+            this.editTemplate = _.template($('#user').html());
+            var self = this;
+            Backbone.on('user-create', function(params) {
+                self.create(params);
+            });
+            Backbone.on('user-save', function(params) {
+                self.save(params);
+            });
+            Backbone.on('user-cancel', function(params) {
+                self.cancel(params);
+            });
+        },
+        create: function() {
+            this.userModel = new App.Model.UserModel();
+            this._renderEdit();
+        },
+        save:function() { 
+            var model = $('#userForm').serializeObject();
+            var nombre= model['firstName'];
+            var apellido= model['lastName'];
+            var fecha1= model['fecha'];
+            
+            var msj= "La persona "+nombre+" " +apellido + " nacio el " + fecha1;
+            
+            var m =document.getElementById('mensaje');
+            var e= document.createElement('p');
+            
+            e.innerHTML= msj;
+            m.appendChild(e);
+            //alert('saved model: '+JSON.stringify(model));
+        },
+        cancel: function(){
+            alert('Cancel');
+        },
+        _renderEdit: function() {
+            var self = this;
+            self.$el.html(self.editTemplate({user: self.userModel}));
+        }
+    });
